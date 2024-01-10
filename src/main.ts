bfa2d774f59a4a46e62d94197441560c4c98ba27
mrsyafapri <rizkisyafapri@gmail.com>
2024-01-10 08:24:56 +0700

    feat: add interceptor for better response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/response/response.interceptor.ts
@@ -0,0 +1,56 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+    return next.handle().pipe(
+      map((res: unknown) => this.responseHandler(res, context)),
+      catchError((err: HttpException) =>
+        throwError(() => this.errorHandler(err, context)),
+      ),
+    );
+  }
+
+  errorHandler(exception: HttpException, context: ExecutionContext) {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      status: false,
+      statusCode: status,
+      endpoint: request.url,
+      message: exception.message,
+      result: exception,
+    });
+  }
+
+  responseHandler(res: any, context: ExecutionContext) {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const statusCode = response.statusCode;
+
+    return {
+      status: true,
+      endpoint: request.url,
+      statusCode,
+      result: res,
+    };
+  }
+}
